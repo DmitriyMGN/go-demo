f5eee4e1db576d698dddb511aea2808f14d56d01
dmitriy <dmitry.mysyagin@yandex.ru>
2026-02-05 19:25:11 +0300

    complete

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 func main() {
-	// const IMTPower = 1
-	// var userHeight, userWeight float64 = 1.93, 100
-	// fmt.Print("Введите свой рост в метрах: ")
-	// fmt.Scan(&userHeight)
-	// fmt.Print("Введите свой вес в кг: ")
-	// fmt.Scan(&userWeight)
 
-	// IMT := userWeight / math.Pow(userHeight, IMTPower)
-	// fmt.Print("Ваш индекс массы тела: ")
-	// fmt.Print(IMT)
+	const usdToEur = 0.85
+	const usdToRub = 0.75
+	const eurToRub = usdToRub / usdToEur
+
 }
