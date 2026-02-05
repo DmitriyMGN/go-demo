963cc10379e83c79c08d79b6ce7ef934b3cff07c
dmitriy <dmitry.mysyagin@yandex.ru>
2026-02-05 19:13:14 +0300

    delete files

--- main.go
+++ main.go
@@ -1,19 +1,14 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
 func main() {
-	const IMTPower = 1
-	var userHeight, userWeight float64 = 1.93, 100
-	fmt.Print("Введите свой рост в метрах: ")
-	fmt.Scan(&userHeight)
-	fmt.Print("Введите свой вес в кг: ")
-	fmt.Scan(&userWeight)
+	// const IMTPower = 1
+	// var userHeight, userWeight float64 = 1.93, 100
+	// fmt.Print("Введите свой рост в метрах: ")
+	// fmt.Scan(&userHeight)
+	// fmt.Print("Введите свой вес в кг: ")
+	// fmt.Scan(&userWeight)
 
-	IMT := userWeight / math.Pow(userHeight, IMTPower)
-	fmt.Print("Ваш индекс массы тела: ")
-	fmt.Print(IMT)
+	// IMT := userWeight / math.Pow(userHeight, IMTPower)
+	// fmt.Print("Ваш индекс массы тела: ")
+	// fmt.Print(IMT)
 }
